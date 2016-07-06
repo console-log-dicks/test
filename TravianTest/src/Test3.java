34dce16866ed84b0170f0c74e19aacf4746f5835
console-log-dicks <wannes.bouwen@student.kuleuven.be>
2016-07-06 23:53:08 +0200

    test with atom

--- TravianTest/src/Test3.java
+++ TravianTest/src/Test3.java
@@ -4,7 +4,8 @@ public class Test3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("dicks");
-		System.out.println("conflictdd ds");
+		System.out.println("conflictdd
+		ds");
 	}
 
 }
